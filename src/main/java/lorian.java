52ad5fa7866bec98638c5324cfe92bc1ee952843
Allan Max <allan@maxmelo.com>
2022-05-16 05:13:31 -0300

    Add files via upload

---
+++ src/main/java/lorian.java
@@ -0,0 +1,9 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class lorian extends JavaPlugin {
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
